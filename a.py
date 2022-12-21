37ccdb7893fc32dbedadd892cdcb371bfad4281f
bg <zzb090317@outlook.com>
2022-12-21 19:21:09 +0800

    a.py

---
+++ a.py
@@ -0,0 +1,10 @@
+'''
+Author: 榜哥
+Date: 2022-12-19 19:14:26
+LastEditors: Please set LastEditors
+LastEditTime: 2022-12-20 07:50:57
+FilePath: \vs\py\a.py
+Description: QQ:2104864320
+Copyright (c) 2022 by 榜哥 email: Zzb090317@outlook.com , All Rights Reserved.
+'''
+print('right')
